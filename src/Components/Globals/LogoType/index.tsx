ac8154b11a0645cb8fe881202d1de960f69c63e8
Stephen Pal-George <stephenngeorge@gmail.com>
2019-06-08 16:16:23 +0100

    LogoType node animation, b&w

--- src/Components/Globals/LogoType/index.tsx
+++ src/Components/Globals/LogoType/index.tsx
@@ -4,8 +4,13 @@ import React from 'react'
 
 const Logo: React.FC = () => {
     return (
-        <div>
+        <div className='logo'>
+            <div className='node' id='node-1'></div>
+            <div className='node' id='node-2'></div>
             <h1>Timelines</h1>
+            <div className='node' id='node-3'></div>
+            <div className='node' id='node-4'></div>
+            <div className='node' id='node-5'></div>
         </div>
     )
 }
