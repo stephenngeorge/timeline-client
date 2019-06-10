44ced69d582bac80d847a8290c99f6686ea25888
Stephen Pal-George <stephenngeorge@gmail.com>
2019-06-10 17:52:04 +0100

    change colour pallette

---
+++ src/Components/Pages/LandingPage/About.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const About: React.FC = () => {
+    return (
+        <div className='about'>
+            <p>
+                Timelines is an elegant, minimalist app for showing 
+                things in chronological order. Use for project management, 
+                holiday planning or revising the British Monarchy...
+            </p>
+        </div>
+    )
+}
+
+export default About
--- src/Components/Pages/LandingPage/index.tsx
+++ src/Components/Pages/LandingPage/index.tsx
@@ -3,14 +3,18 @@ import './page.css'
 import React from 'react'
 
 // component imports
+import About from './About'
 import { LogoType } from '../../Globals'
 import { Login } from '../../Forms'
 
 const LandingPage: React.FC = () => {
     return (
         <div className='landing-page'>
-            <LogoType />
-            <Login />
+            <About />
+            <div className='login-wrapper'>
+                <LogoType />
+                <Login />
+            </div>
         </div>
     )
 }
