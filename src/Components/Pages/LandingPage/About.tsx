67b989f22481890d34d9c6e3832dd70b8d1b324c
Stephen Pal-George <stephenngeorge@gmail.com>
2019-06-10 19:02:45 +0100

    border colour change on login button

--- src/Components/Pages/LandingPage/About.tsx
+++ src/Components/Pages/LandingPage/About.tsx
@@ -4,9 +4,10 @@ const About: React.FC = () => {
     return (
         <div className='about'>
             <p>
-                Timelines is an elegant, minimalist app for showing 
-                things in chronological order. Use for project management, 
-                holiday planning or revising the British Monarchy...
+                Timelines is an elegant, minimalist app for plotting 
+                things in chronological order or a particular sequence. 
+                Use for project management, holiday planning or 
+                revising the British Monarchy...
             </p>
         </div>
     )
