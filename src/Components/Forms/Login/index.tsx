c086a977dbf8b84dfb36cffefe35690cce7fa0f5
Stephen Pal-George <stephenngeorge@gmail.com>
2019-06-10 16:04:50 +0100

    login form styles, key asset

--- src/Components/Forms/Login/index.tsx
+++ src/Components/Forms/Login/index.tsx
@@ -1,6 +1,6 @@
 import './login.css'
 
-import React, { useContext, useState } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 import { AuthContext, types } from '../../../store'
@@ -9,12 +9,34 @@ import { authQueries } from '../../../queries'
 // component imports
 import { LogoType } from '../../Globals'
 
+// asset imports
+import { key_icon } from '../../../assets'
+
 const LoginForm: React.FC = () => {
 
     const { dispatch } = useContext(AuthContext)
 
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
+    const [disabled, setDisabled] = useState(true)
+
+    useEffect(() => {
+        let usernameNode = document.querySelector('#username-node')
+        let passwordNode = document.querySelector('#password-node')
+        let loginButton = document.querySelector('#login-button')
+
+        if (usernameNode !== null && passwordNode !== null && loginButton !== null) {
+            username === '' ? usernameNode.classList.remove('complete') : usernameNode.classList.add('complete')
+            password === '' ? passwordNode.classList.remove('complete') : passwordNode.classList.add('complete')
+
+            if (usernameNode.classList.contains('complete') && passwordNode.classList.contains('complete')) {
+                setDisabled(false)
+            }
+            else {
+                setDisabled(true)
+            }
+        }
+    }, [username, password])
 
     const handleSubmit = async () => {
         const loginData = await authQueries.login(username, password)
@@ -39,7 +61,7 @@ const LoginForm: React.FC = () => {
                             value={ username }
                             onChange={ (e: any) => setUsername(e.target.value) }
                     />
-                    <label htmlFor='username'>username:</label>
+                    <label htmlFor='username'>username</label>
                 </div>
 
                 <div className='form-group'>
@@ -49,10 +71,16 @@ const LoginForm: React.FC = () => {
                             value={ password }
                             onChange={ (e:any) => setPassword(e.target.value) }
                     />
-                    <label htmlFor='password'>password:</label>
+                    <label htmlFor='password'>password</label>
                 </div>
 
-                <button type='submit'>login</button>
+                <div className='timeline-button'>
+                    <div className='button-node' id='username-node'></div>
+                    <div className='button-node' id='password-node'></div>
+                    <button disabled={ disabled } type='submit' id='login-button'>
+                        <img src={ key_icon } alt='login' id='login-icon' />
+                    </button>
+                </div>
 
                 <Link to='/signup'>create account</Link>
             </form>
---
+++ src/assets/index.tsx
@@ -0,0 +1,5 @@
+import key_icon from './key_icon.png'
+
+export {
+    key_icon
+}
