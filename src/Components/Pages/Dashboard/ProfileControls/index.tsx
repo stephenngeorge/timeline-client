1e9679f09e02a0beffdbbf4a6d822ecfa1461b92
Stephen Pal-George <stephenngeorge@gmail.com>
2019-06-11 16:13:13 +0100

    profile controls label appears

--- src/Components/Pages/Dashboard/ProfileControls/index.tsx
+++ src/Components/Pages/Dashboard/ProfileControls/index.tsx
@@ -1,6 +1,7 @@
 import './profilecontrols.css'
+import './typography.css'
 
-import React, { Dispatch, SetStateAction } from 'react'
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
 
 // asset imports
 import { create_icon, edit_icon, settings_icon } from '../../../../assets'
@@ -11,8 +12,45 @@ interface IProfileControlsProps {
 }
 
 const ProfileControls: React.FC<IProfileControlsProps> = ({setAddTimelineForm, addTimelineForm}) => {
+    const [label, setLabel] = useState('')
+
+    useEffect(() => {
+        const   createTimelineButton = document.getElementById('button-createTimeline'),
+                editProfileButton = document.getElementById('button-editProfile'),
+                accountSettingsButton = document.getElementById('button-accountSettings')
+
+        const   timelineLabel = () => setLabel('create timeline'),
+                editLabel = () => setLabel('edit profile'),
+                settingsLabel = () => setLabel('account settings'),
+                removeLabel = () => setLabel('')
+
+        if (createTimelineButton !== null && editProfileButton !== null && accountSettingsButton !== null) {
+            createTimelineButton.addEventListener('mouseover', timelineLabel)
+            editProfileButton.addEventListener('mouseover', editLabel)
+            accountSettingsButton.addEventListener('mouseover', settingsLabel)
+
+            createTimelineButton.addEventListener('mouseout', removeLabel)
+            editProfileButton.addEventListener('mouseout', removeLabel)
+            accountSettingsButton.addEventListener('mouseout', removeLabel)
+        }
+
+        return () => {
+            if (createTimelineButton !== null && editProfileButton !== null && accountSettingsButton !== null) {
+                createTimelineButton.removeEventListener('mouseover', timelineLabel)
+                createTimelineButton.removeEventListener('mouseout', removeLabel)
+
+                editProfileButton.removeEventListener('mouseover', editLabel)
+                editProfileButton.removeEventListener('mouseout', removeLabel)
+
+                accountSettingsButton.removeEventListener('mouseover', settingsLabel)
+                accountSettingsButton.removeEventListener('mouseout', removeLabel)
+            }
+        }
+    }, [])
+
     return (
         <div className='profile-header__controls'>
+            <p id='controls-label'>{ label }</p>
             <button id='button-createTimeline' onClick={ () => setAddTimelineForm(!addTimelineForm) }>
                 <img src={ create_icon } alt='add timeline' />
             </button>
