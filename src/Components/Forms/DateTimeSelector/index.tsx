3da3ef67c6236039d7b26284dedd71a3e1f5fc4e
Stephen Pal-George <stephenngeorge@gmail.com>
2019-06-19 13:40:24 +0100

    date picker width expanded for form timeline

--- src/Components/Forms/DateTimeSelector/index.tsx
+++ src/Components/Forms/DateTimeSelector/index.tsx
@@ -28,44 +28,49 @@ const DateTimeSelector: React.FC<IDateTimeSelectorProps> = ({ timelineTitle }) =
         dashboardProps.dateTimeSelector === true ? animation = 'slide-in' : animation = 'slide-out'
     }
     return (
-        <form className={`date-time-selector ${ animation }`}>
-            <h3 className='timeline-title'>{ timelineTitle }</h3>
-            <div className='months'>
-                {
-                    months.map(month => {
-                        const chosenMonth = selectedMonth.monthShort === month.monthShort ? 'chosen-month' : ''
-                        return <div onClick={() => setSelectedMonth(month)} key={ month.monthShort } className={`month_single ${chosenMonth}`}>{month.monthShort}</div>
-                    })
-                }
-            </div>
-            <div className='days'>
-                {
-                    days.map(day => {
-                        const chosenDay = selectedDay === day ? 'chosen-day' : ''
-                        return <p onClick={ () => setSelectedDay(day) } className={`day_single ${chosenDay}`} key={day}>{day + 1}</p>
-                    })
-                }
-            </div>
-            <div className='time'>
-                <div className='date-form-group'>
-                    <input  name='hours'
-                            id='hours'
-                            type='text'
-                            value={ hours }
-                            onChange={ (e:any) => setHours(e.target.value) }
-                    />
+        <div className={`date-time-selector_wrapper ${ animation }`}>
+            <form className='date-time-selector'>
+                <h3 className='timeline-title'>{ timelineTitle } deadline</h3>
+                <div className='months'>
+                    {
+                        months.map(month => {
+                            const chosenMonth = selectedMonth.monthShort === month.monthShort ? 'chosen-month' : ''
+                            return <div onClick={() => setSelectedMonth(month)} key={ month.monthShort } className={`month_single ${chosenMonth}`}>{month.monthShort}</div>
+                        })
+                    }
                 </div>
-                <p>:</p>
-                <div className='date-form-group'>
-                    <input  name='minutes'
-                            id='minutes'
-                            type='text'
-                            value={ minutes }
-                            onChange={ (e:any) => setMinutes(e.target.value) }
-                    />
+                <div className='days'>
+                    {
+                        days.map(day => {
+                            const chosenDay = selectedDay === day ? 'chosen-day' : ''
+                            return <p onClick={ () => setSelectedDay(day) } className={`day_single ${chosenDay}`} key={day}>{day + 1}</p>
+                        })
+                    }
                 </div>
-            </div>
-        </form>
+                <div className='time'>
+                    <div className='date-form-group'>
+                        <input  name='hours'
+                                id='hours'
+                                type='text'
+                                value={ hours }
+                                onChange={ (e:any) => setHours(e.target.value) }
+                        />
+                        <label htmlFor='hours'>HH</label>
+                    </div>
+                    <p>:</p>
+                    <div className='date-form-group'>
+                        <input  name='minutes'
+                                id='minutes'
+                                type='text'
+                                value={ minutes }
+                                onChange={ (e:any) => setMinutes(e.target.value) }
+                        />
+                        <label htmlFor='minutes'>MM</label>
+                    </div>
+                    <p className='tip'>use 24hr clock times</p>
+                </div>
+            </form>
+        </div>
     )
 }
 
